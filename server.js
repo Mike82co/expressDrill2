a33a7c7878bb36a0d2a5c02f47d0168867230a13
Mike Lavin <mikelavin@MacBook-Pro-5.local>
2018-06-08 14:30:40 -0600

    completed drill 2 deploy on heroku

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require ('express');
+const cors = require('cors')
+const app = express();
+const data =require('./instructors')
+const port = process.env.PORT || 3000;
+
+app.listen(port)
+
+app.get('/', function (request, response){
+    response.json({ data : data})
+})
+
+app.get('/:ID', function(request,response, next){
+    var instructor = getInstructor(request.params.ID, data)
+    if (!instructor){
+        response.status(404).json({error:{
+            message: 'Try again'
+        }})
+    }
+    else{
+        response.json(instructor)
+    }
+})
+
+function getInstructor(id, dataArr){
+    var test = dataArr.filter( instructor => {
+        if(instructor.ID == id){
+            return instructor
+        }
+        else{
+            return null
+        }
+    })
+    return test[0]
+}
