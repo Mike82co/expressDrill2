296626a5b42b5488745e30d984bd08e27496282d
Mike Lavin <mikelavin@MacBook-Pro-8.Home>
2018-07-03 18:37:49 -0600

    Removed semi-colons. Couldnt find an attribute that was not camel case

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require ('express');
+const express = require ('express')
 const cors = require('cors')
-const app = express();
+const app = express()
 const data =require('./instructors')
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 app.listen(port)
 
